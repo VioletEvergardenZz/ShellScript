6f7484296653781f2814160898a90f24e6086659
VioletEvergardenZz <1561862432@qq.com>
2024-06-12 20:42:08 +0800

    commit

--- StartStopServices.sh
+++ StartStopServices.sh
@@ -4,7 +4,7 @@
 #  启动/停止服务：编写一个脚本以同时启动或停止多个服务。可以使用 systemctl 命令和循   环结构来实现。
 
 
-
+# 数组
 services=("service1" "service2" "service3")
 action="start"  # 或者 "stop"
 for service in "${services[@]}"; do
